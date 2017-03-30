a5675aca1d95e99e75a516b559da89ef6efc234b
James Warren Basham <jwbasham@outlook.com>
2017-03-30 13:37:00 -0700

    redirected landing page for signed in users to the file list while still showing the marketing page when not signed up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
+	devise_for :users
+  	get 'welcome/index'
 
-  root 'welcome#index'
+  	root 'welcome#index'
 
-  resources :docs
+  	resources :docs
+  	authenticated :user do
+  		root "docs#index", as: "authenticated_root" 
+  	end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
