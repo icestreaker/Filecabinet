83c87b1362d76c00253c797f57ea00b571e727f9
James Warren Basham <jwbasham@outlook.com>
2017-03-30 21:18:18 -0700

    1

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   	resources :docs
   	authenticated :user do
-  		root "docs", as: "authenticated_root" 
+  		root 'docs#index', as: 'authenticated_root' 
   	end
 
 
